f9e90339a97e74da7d8c0fc851c36295efe3c33d
mendebian <lucasmoraismendes@gmail.com>
2024-11-13 18:07:34 -0300

    client-side prediction/interpolation

--- script.js
+++ script.js
@@ -4,149 +4,53 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const playerRadius = 20;
+const PLAYER_SPEED = 2.4;
 let players = {};
-let localPlayer = { x: canvas.width / 2, y: canvas.height / 2, id: null, isHost: false };
-let score = { home: 0, away: 0 };
+let localPlayer = { id: null, x: canvas.width / 2, y: canvas.height / 2, velocity: { x: 0, y: 0 }, isHost: false };
 let peer = new Peer();
 let connections = [];
 let hostId = null;
-let speed = 2.4;
-let directionAngle = 0;
 
-const { Engine, World, Bodies, Body } = Matter;
-const engine = Engine.create();
-const world = engine.world;
-world.gravity.y = 0;
-const playerBodies = {};
+const TICK_RATE = 30;
+const TICK_INTERVAL = 1000 / TICK_RATE;
+let lastTick = Date.now();
 
-// PeerJS Setup
 peer.on("open", id => {
   localPlayer.id = id;
   const roomId = prompt("Insira o ID da sala para se conectar ou deixe vazio para criar uma nova:");
-
-  if (roomId) {
-    connectToHost(roomId);
-  } else {
-    createRoomAsHost();
-  }
+  roomId ? connectToHost(roomId) : createRoomAsHost();
 });
 
-// Host Functions
 function createRoomAsHost() {
   hostId = localPlayer.id;
   localPlayer.isHost = true;
-  displayRoomId(localPlayer.id);
-
-  players[localPlayer.id] = { x: localPlayer.x, y: localPlayer.y, id: localPlayer.id };
-  addPlayerToPhysics(localPlayer);
+  players[localPlayer.id] = { ...localPlayer };
 
   peer.on("connection", connection => {
     connections.push(connection);
 
     connection.on("open", () => {
-      connection.send({ type: "init", players, score });
+      connection.send({ type: "init", players });
     });
 
-    connection.on("data", data => {
-      handleHostData(data, connection);
-    });
-
-    connection.on("close", () => {
-      const disconnectedPlayer = connections.find(c => c.peer === connection.peer);
-      if (disconnectedPlayer) {
-        connections.splice(connections.indexOf(disconnectedPlayer), 1);
-        handleHostData({ type: "playerDisconnected", playerId: connection.peer });
-      }
-    });
+    connection.on("data", data => handleHostData(data, connection));
+    connection.on("close", () => removeConnection(connection.peer));
   });
 
-  // Sync positions periodically
-  setInterval(() => {
-    if (localPlayer.isHost) {
-      syncHostPositions();
-      broadcast({ type: "updatePlayers", players });
-    }
-  }, 50);
+  displayRoomId(localPlayer.id);
 }
 
-function handleHostData(data, connection) {
-  switch (data.type) {
-    case "newPlayer":
-      players[data.player.id] = { x: data.player.x, y: data.player.y, id: data.player.id };
-      addPlayerToPhysics(data.player);
-      broadcast({ type: "updatePlayers", players });
-      break;
-
-    case "playerAction":
-      const player = players[data.playerId];
-      if (data.action === "move") {
-        const body = playerBodies[data.playerId];
-        if (body) {
-          Body.setVelocity(body, { x: data.deltaX, y: data.deltaY });
-          Body.setPosition(body, { x: data.x, y: data.y });
-          syncHostPositions();
-        }
-        broadcast({ type: "updatePlayers", players });
-      }
-      break;
-
-    case "playerDisconnected":
-      delete players[data.playerId];
-      World.remove(world, playerBodies[data.playerId]);
-      delete playerBodies[data.playerId];
-      broadcast({ type: "updatePlayers", players });
-      break;
-  }
-}
-
-// Client Functions
-function connectToHost(hostRoomId) {
-  const conn = peer.connect(hostRoomId);
-  connections.push(conn);
-
+function connectToHost(roomId) {
+  const conn = peer.connect(roomId);
   conn.on("open", () => {
+    connections.push(conn);
     conn.send({ type: "newPlayer", player: { id: localPlayer.id, x: localPlayer.x, y: localPlayer.y } });
+
     conn.on("data", handleClientData);
-  });
-
-  conn.on("close", () => {
-    alert("Você foi desconectado do host.");
-    players = {};
+    conn.on("close", () => alert("Você foi desconectado do host."));
   });
 }
 
-function handleClientData(data) {
-  switch (data.type) {
-    case "init":
-      players = data.players;
-      score = data.score;
-
-      Object.values(players).forEach(player => {
-        if (!playerBodies[player.id]) {
-          addPlayerToPhysics(player);
-        }
-      });
-      break;
-
-    case "updatePlayers":
-      players = data.players;
-
-      Object.values(players).forEach(player => {
-        const body = playerBodies[player.id];
-        if (body) {
-          Body.setPosition(body, { x: player.x, y: player.y });
-          Body.setVelocity(body, { x: 0, y: 0 });
-        }
-      });
-      break;
-
-    case "updateScore":
-      score = data.score;
-      break;
-  }
-}
-
-// Utility Functions
 function displayRoomId(id) {
   const input = document.createElement("input");
   input.value = id;
@@ -157,93 +61,137 @@ function displayRoomId(id) {
   document.body.appendChild(input);
 }
 
-function addPlayerToPhysics(player) {
-  const body = Bodies.circle(player.x, player.y, playerRadius, {
-    restitution: 0.8,
-    friction: 0.1,
-    frictionAir: 0.05,
-    label: player.id,
-  });
-  playerBodies[player.id] = body;
-  World.add(world, body);
-}
-
-function syncHostPositions() {
-  Object.values(players).forEach(player => {
-    const body = playerBodies[player.id];
-    if (body) {
-      player.x = body.position.x;
-      player.y = body.position.y;
-    }
-  });
-}
-
-function sendPlayerAction(actionType, deltaX = 0, deltaY = 0) {
+function sendPlayerAction(deltaX, deltaY) {
   if (localPlayer.isHost) {
-    handleHostData({
-      type: "playerAction",
-      playerId: localPlayer.id,
-      action: actionType,
-      deltaX,
-      deltaY,
-      x: playerBodies[localPlayer.id].position.x,
-      y: playerBodies[localPlayer.id].position.y,
-    });
+    applyPlayerAction(localPlayer.id, deltaX, deltaY);
+    broadcast({ type: "playerAction", playerId: localPlayer.id, deltaX, deltaY });
   } else {
     const conn = connections[0];
     if (conn && conn.open) {
-      conn.send({
-        type: "playerAction",
-        playerId: localPlayer.id,
-        action: actionType,
-        deltaX,
-        deltaY,
-        x: playerBodies[localPlayer.id].position.x,
-        y: playerBodies[localPlayer.id].position.y,
-      });
+      conn.send({ type: "playerAction", playerId: localPlayer.id, deltaX, deltaY });
     }
   }
 }
 
-// Input Events
+function broadcast(data) {
+  connections.forEach(conn => {
+    if (conn.open) conn.send(data);
+  });
+}
+
+function applyPlayerAction(playerId, deltaX, deltaY) {
+  const player = players[playerId];
+  if (!player) return;
+
+  if (playerId === localPlayer.id) {
+    player.velocity.x = deltaX;
+    player.velocity.y = deltaY;
+
+    player.x += deltaX;
+    player.y += deltaY;
+  } else {
+    player.velocity.x = deltaX;
+    player.velocity.y = deltaY;
+
+    player.x += deltaX;
+    player.y += deltaY;
+  }
+}
+
 canvas.addEventListener("touchmove", e => {
   const touchX = e.touches[0].clientX;
   const touchY = e.touches[0].clientY;
 
+  // Calcular o ângulo
   const deltaX = touchX - localPlayer.x;
   const deltaY = touchY - localPlayer.y;
   directionAngle = Math.atan2(deltaY, deltaX);
 
-  const moveX = speed * Math.cos(directionAngle);
-  const moveY = speed * Math.sin(directionAngle);
+  const moveX = PLAYER_SPEED * Math.cos(directionAngle);
+  const moveY = PLAYER_SPEED * Math.sin(directionAngle);
 
-  const body = playerBodies[localPlayer.id];
-  if (body) {
-    Body.setVelocity(body, { x: moveX, y: moveY });
-  }
+  localPlayer.x += moveX;
+  localPlayer.y += moveY;
 
-  sendPlayerAction("move", moveX, moveY);
+  sendPlayerAction(moveX, moveY);
+
+  touchStartX = touchX;
+  touchStartY = touchY;
 });
 
-// Rendering
 function renderGame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  ctx.beginPath();
+  ctx.arc(localPlayer.x, localPlayer.y, playerRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "green";
+  ctx.fill();
+  ctx.closePath();
+
   Object.values(players).forEach(player => {
-    ctx.beginPath();
-    ctx.arc(player.x, player.y, playerRadius, 0, Math.PI * 2);
-    ctx.fillStyle = player.id === localPlayer.id ? "green" : "blue";
-    ctx.fill();
-    ctx.closePath();
+    if (player.id !== localPlayer.id) {
+      const prevPos = player.lastPosition || { x: player.x, y: player.y };
+      const interpolationFactor = 0.1;
+      const interpolatedX = prevPos.x + (player.x - prevPos.x) * interpolationFactor;
+      const interpolatedY = prevPos.y + (player.y - prevPos.y) * interpolationFactor;
+
+      ctx.beginPath();
+      ctx.arc(interpolatedX, interpolatedY, playerRadius, 0, Math.PI * 2);
+      ctx.fillStyle = "blue";
+      ctx.fill();
+      ctx.closePath();
+
+      player.lastPosition = { x: interpolatedX, y: interpolatedY };
+    }
   });
 }
 
-// Game Loop
-(function gameLoop() {
-  Engine.update(engine);
-  if (localPlayer.isHost) {
-    syncHostPositions();
+function handleHostData(data, connection) {
+  switch (data.type) {
+    case "newPlayer":
+      players[data.player.id] = { x: data.player.x, y: data.player.y, id: data.player.id };
+      broadcast({ type: "updatePlayers", players });
+      break;
+
+    case "playerAction":
+      const player = players[data.playerId];
+      if (player) {
+        player.x += data.deltaX;
+        player.y += data.deltaY;
+        broadcast({ type: "updatePlayers", players });
+      }
+      break;
   }
+}
+
+function handleClientData(data) {
+  switch (data.type) {
+    case "init":
+      players = data.players;
+      break;
+
+    case "updatePlayers":
+      players = data.players;
+      break;
+
+    case "playerAction":
+      const player = players[data.playerId];
+      if (player) {
+        player.x += data.deltaX;
+        player.y += data.deltaY;
+      }
+      break;
+  }
+}
+
+function gameLoop() {
+  if (!localPlayer.isHost) {
+    localPlayer.x += localPlayer.velocity.x;
+    localPlayer.y += localPlayer.velocity.y;
+  }
+
   renderGame();
   requestAnimationFrame(gameLoop);
-})();
+}
+
+gameLoop();
